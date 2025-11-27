63050fd748d751508e21496683bd5df2e25a38bb
LeandroPalinski <211562687+LeandroPalinski@users.noreply.github.com>
2025-11-27 09:49:35 -0500

    fix: Corrigir URL e credenciais do Supabase

--- src/lib/supabaseClient.jsx
+++ src/lib/supabaseClient.jsx
@@ -1,7 +1,7 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lzjuwqfgnviomdbjfvif.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6anV3cWZnbnZpb21kYmpmdmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNjExNjQsImV4cCI6MjA2MjczNzE2NH0.TDoTKQmzx6Uc2UmbWrWvCSjTZC9bM9kHPH95q5GIv-s";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://zgymykkrtsrxmxiqqcbi.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpneW15a2tydHNyeG14aXFxY2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2NzM3MTMsImV4cCI6MjA0ODI0OTcxM30.Xr3VqQWqHMx7Gg0_WxKxQOvCJLPF8Ov8_KGBmYqPqhI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
